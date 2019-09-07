2ab2bee0cff4d8554b9c1807807c3ac5cfb6e94b
erwanledoux <lawanledoux@gmail.com>
2019-09-07 17:13:23 +0200

    added tests for path with several params

--- src/__specs__/getForm.spec.js
+++ src/__specs__/getForm.spec.js
@@ -2,7 +2,7 @@ import getForm from '../getForm'
 
 describe('getForm', () => {
   describe('when location, match are in a readonly state', () => {
-    it('should return a readonly form', () => {
+    it('should return a readonly form for a simple path', () => {
       // given
       const location = {
         pathname: "/foos/AE",
@@ -30,10 +30,40 @@ describe('getForm', () => {
       })
       expect(form.getReadOnlyUrl("AE")).toBe("/foos/AE")
     })
+
+    it('should return a readonly form with a complex path with several params', () => {
+      // given
+      const location = {
+        pathname: "/foos/AE/bars/BF",
+        search: ''
+      }
+      const match = {
+        params: {
+          barId: "BF",
+          fooId: "AE"
+        }
+      }
+
+      // when
+      const form = getForm({ location, match })
+
+      // then
+      expect(form).toStrictEqual({
+        creationUrl: "/foos/AE/bars/creation",
+        getReadOnlyUrl: expect.any(Function),
+        id: "BF",
+        isCreatedEntity: false,
+        isModifiedEntity: false,
+        method: undefined,
+        modificationUrl: "/foos/AE/bars/BF/modification",
+        readOnly: true
+      })
+      expect(form.getReadOnlyUrl("BF")).toBe("/foos/AE/bars/BF")
+    })
   })
 
   describe('when location, match are in a creation state', () => {
-    it('should return a creation form', () => {
+    it('should return a creation form for a simple path', () => {
       // given
       const location = {
         pathname: "/foos/creation",
@@ -61,10 +91,40 @@ describe('getForm', () => {
       })
       expect(form.getReadOnlyUrl("AE")).toBe("/foos/AE")
     })
+
+    it('should return a creation form for a complex path with several params', () => {
+      // given
+      const location = {
+        pathname: "/foos/AE/bars/creation",
+        search: ''
+      }
+      const match = {
+        params: {
+          barId: "creation",
+          fooId: "AE"
+        }
+      }
+
+      // when
+      const form = getForm({ location, match })
+
+      // then
+      expect(form).toStrictEqual({
+        creationUrl: "/foos/AE/bars/creation",
+        getReadOnlyUrl: expect.any(Function),
+        id: "creation",
+        isCreatedEntity: true,
+        isModifiedEntity: false,
+        method: "POST",
+        modificationUrl: undefined,
+        readOnly: false
+      })
+      expect(form.getReadOnlyUrl("BF")).toBe("/foos/AE/bars/BF")
+    })
   })
 
   describe('when location, match are in a modification state', () => {
-    it('should return a modification form', () => {
+    it('should return a modification form for a simple path', () => {
       // given
       const location = {
         pathname: "/foos/AE/modification",
@@ -92,6 +152,37 @@ describe('getForm', () => {
         readOnly: false
       })
       expect(form.getReadOnlyUrl("AE")).toBe("/foos/AE")
+    })
+
+    it('should return a modification form for a complex path with several params', () => {
+      // given
+      const location = {
+        pathname: "/foos/AE/bars/BF/modification",
+        search: ''
+      }
+      const match = {
+        params: {
+          barId: "BF",
+          fooId: "AE",
+          modification: "modification"
+        }
+      }
+
+      // when
+      const form = getForm({ location, match })
+
+      // then
+      expect(form).toStrictEqual({
+        creationUrl: "/foos/AE/bars/creation",
+        getReadOnlyUrl: expect.any(Function),
+        id: "BF",
+        isCreatedEntity: false,
+        isModifiedEntity: true,
+        method: "PATCH",
+        modificationUrl: "/foos/AE/bars/BF/modification",
+        readOnly: false
+      })
+      expect(form.getReadOnlyUrl("BF")).toBe("/foos/AE/bars/BF")
     })
   })
 
